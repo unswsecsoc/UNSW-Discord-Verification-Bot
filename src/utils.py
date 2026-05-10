8c67c45655f022907fe6c1f12e6e11f13036d7c2
Cameron McDonald <59149640+mcdcam@users.noreply.github.com>
2026-05-11 01:45:07 +1000

    Refactoring bot.py

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,18 @@
+import hashlib
+import json
 import logging
 import os
+import sqlite3
+from functools import lru_cache
+from typing import TYPE_CHECKING
 
 import discord
 import logfire
 
 import config
+
+if TYPE_CHECKING:
+    from discord.app_commands import CommandTree
 
 
 def get_guild_dir(guild: discord.Guild):
@@ -24,6 +32,62 @@ def save_guild_info(guild: discord.Guild) -> None:
         f.write(guild.name)
 
 
+@lru_cache(maxsize=16)
+def get_guild_db(guild: discord.Guild):
+    path = get_guild_db_path(guild)
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    conn = sqlite3.connect(path)
+    c = conn.cursor()
+    c.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            discord_id INTEGER PRIMARY KEY,
+            email TEXT,
+            verified INTEGER NOT NULL CHECK (verified IN (0, 1)) DEFAULT 0,
+            verified_at INTEGER CHECK (verified_at > 0),
+            CHECK ((verified_at IS NULL) OR verified) -- verified_at implies verified
+        ) STRICT
+    """)
+    c.execute("""
+        CREATE TABLE IF NOT EXISTS config (
+            key TEXT PRIMARY KEY,
+            value TEXT NOT NULL
+        ) STRICT
+    """)
+    conn.commit()
+    logging.info(f"loaded or created database for guild {guild.id}")
+
+    save_guild_info(guild)
+
+    return conn
+
+
+def get_verified_role(guild: discord.Guild) -> discord.Role | None:
+    conn = get_guild_db(guild)
+    c = conn.cursor()
+    c.execute("SELECT value FROM config WHERE key = 'verified_role_id'")
+    row = c.fetchone()
+
+    if row is None:
+        return None
+
+    role_id = int(row[0])
+    return guild.get_role(role_id)
+
+
+def set_verified_role(guild: discord.Guild, role: discord.Role) -> None:
+    conn = get_guild_db(guild)
+    c = conn.cursor()
+    c.execute(
+        """
+        INSERT INTO config (key, value)
+        VALUES ('verified_role_id', ?)
+        ON CONFLICT(key) DO UPDATE SET value=excluded.value
+    """,
+        (str(role.id),),
+    )
+    conn.commit()
+
+
 async def log_admin(message, guild, **kwargs):
     logfire.debug(f'log_admin: logging "{message}" to guild {guild.id}')
 
@@ -38,3 +102,30 @@ async def log_admin(message, guild, **kwargs):
         return
 
     await channel.send(message, **kwargs)
+
+
+def get_commands_hash(tree: CommandTree) -> str:
+    # Changes when a commands name or description, or its parameters' name or description changes
+    # Also changes if a parameter's mandatoriness changes
+    commands = sorted(
+        (
+            {
+                "name": c.name,
+                "description": c.description,  # type: ignore
+                "parameters": sorted(
+                    (
+                        {
+                            "name": p.name,
+                            "description": p.description,
+                            "required": p.required,
+                        }
+                        for p in c.parameters  # type: ignore
+                    ),
+                    key=lambda p: p["name"],
+                ),
+            }
+            for c in tree.get_commands()
+        ),
+        key=lambda c: c["name"],
+    )
+    return hashlib.md5(json.dumps(commands, sort_keys=True).encode()).hexdigest()
